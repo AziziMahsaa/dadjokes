58e0a2fd3d1b644d3f49bccd53f420d60f754e09
MahsaAzizi1991 <mahsa.azizi.chenar.1370@gmail.com>
2023-04-18 13:07:16 +0300

    first commit

--- src/App copy.js
+++ src/App copy.js
@@ -3,7 +3,7 @@ import { Row, Button } from "react-bootstrap";
 import { BigText } from "./BigText";
 import { useState } from "react";
 
-const jokes = ["Joke 1", "Joke 2", "Joke 3", "Joke 4", "Joke 5"];
+const jokes = ["Joke 1", "Joke 2", "Joke 3"];
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -16,7 +16,7 @@ function getANewJoke() {
 }
 
 function App() {
-  let [myjoke, setMyjok] = useState(getANewJoke());
+  let [dadjoke, setDadjoke] = useState(getANewJoke());
   return (
     <div className="App">
       <section>
@@ -24,13 +24,14 @@ function App() {
           <h3 className="firstH">Don't laugh challenge</h3>
         </Row>
         <Row>
-          <BigText text={myjoke} />
+          <BigText text={dadjoke} />
         </Row>
         <Row>
           <Button
             className="NewJbutton"
             onClick={function () {
-              setMyjok(getANewJoke());
+              var mamad = getANewJoke();
+              setDadjoke(mamad);
             }}
           >
             Get Another Joke
---
+++ src/BigText.js
@@ -0,0 +1,3 @@
+export function BigText(props) {
+  return <h1 className="mainH">{props.text}</h1>;
+}
